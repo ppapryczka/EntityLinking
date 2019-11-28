4f3c4e4462bb0395afdca963cc015be5cfa84a8e
ppapryczka <patryk.pankiewicz369@gmail.com>
2019-11-28 01:27:34 +0100

    Added request for given token by pywikibot.

--- wikidata_attributes.py
+++ wikidata_attributes.py
@@ -1,9 +1,14 @@
 from wikidata.client import Client
 
-INSTANCE_OF_ID = "P31"
+from typing import List, Dict, Any
+import requests
+import pywikibot
 
+ID_INSTANCE_OF = "P31"
+WIKIDATA_URL = 'https://query.wikidata.org/sparql'
+DEFAULT_RESULTS_LIMIT = 2
 
-def get_data_for_given_entity(entity: str) -> dict:
+def get_data_for_given_entity(entity: str) -> Dict[str, Any]:
     # load data
     client = Client()
     entity = client.get(entity, load=True)
@@ -15,7 +20,7 @@ def get_data_for_given_entity(entity: str) -> dict:
     descriptions = [entity_data['descriptions']["pl"], entity_data['descriptions']["en"]]
 
     instance_of = []
-    for _, obj in enumerate(entity_data["claims"][INSTANCE_OF_ID]):
+    for _, obj in enumerate(entity_data["claims"][ID_INSTANCE_OF]):
         instance_of.append(obj["mainsnak"]["datavalue"]["value"]["id"])
 
     return {
@@ -25,6 +30,39 @@ def get_data_for_given_entity(entity: str) -> dict:
         "instance of": instance_of
     }
 
+def get_results_for_given_token(token: str) -> Any:
+
+
+    site = pywikibot.Site("pl", "wikipedia")
+    page = pywikibot.Page(site, token)
+    item = pywikibot.ItemPage.fromPage(page)
+
+    print(item)
+
+def get_pages_ids_for_given_token(token: str) -> List[str]:
+    # crete request for given token, ref:
+    # https://www.mediawiki.org/wiki/Wikidata_Query_Service/User_Manual/MWAPI#Find_all_entities_with_labels_%22cheese%22_and_get_their_types
+    query = "SELECT * WHERE { " \
+            "   SERVICE wikibase:mwapi { "\
+            "       bd:serviceParam wikibase:api \"EntitySearch\" . " \
+            "       bd:serviceParam wikibase:endpoint \"www.wikidata.org\" . " \
+            f"      bd:serviceParam mwapi:search \"{token}\" . " \
+            f"      bd:serviceParam mwapi:language \"pl\" . " \
+            "       ?item wikibase:apiOutputItem mwapi:item . " \
+            "       ?num wikibase:apiOrdinal true . " \
+            "   } " \
+            "} ORDER BY ASC(?num)"f" LIMIT {DEFAULT_RESULTS_LIMIT}"
+
+    # request for json
+    r = requests.get(WIKIDATA_URL, params={'format': 'json', 'query': query})
+    data = r.json()
+
+    entities = []
+    for x in data["results"]["bindings"]:
+        entities.append(x["item"]["value"].split("/")[-1])
+
+    return entities
 
 if __name__=="__main__":
-    print(get_data_for_given_entity('Q231593'))
+    print(get_pages_ids_for_given_token("Nowy Targ"))
+
