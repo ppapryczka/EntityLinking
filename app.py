52a2558fffb1b8ed09c5df8594b8865ae2f7b22e
Daniel Bigos <daniel.bigos@icloud.com>
2020-01-09 15:26:48 +0100

    feat: add logger

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from entity_linking import wikidata_api as api
 from entity_linking import wikidata_graph as graph
 
+from entity_linking.maintenance.logger import get_logger
 
 def main():
-    print("Hello world")
+    logger = get_logger("app")
+    logger.info("Hello world!")
 
 
 if __name__ == "__main__":
