c5088fb7d12a1d45b8b486be28e260a98c510020
Daniel Bigos <daniel.bigos@icloud.com>
2020-01-12 22:45:39 +0100

    feat: add parser (morfeusz wrapper)

--- entity_linking/parser/parser.py
+++ entity_linking/parser/parser.py
@@ -1,2 +1,23 @@
-def parse_file(filename):
-    return ["test"]
+from morfeusz2 import Morfeusz
+from typing import List
+from entity_linking.grammar.token import Token
+
+
+class Parser():
+    def __init__(self):
+        self._morfeusz = Morfeusz()
+
+    def parse_text(self, text: str):
+        tokens = dict()
+        analyses = self.analyse_text(text)
+        for i, j, analysis in analyses:
+            if (i, j) in tokens.keys():
+                tokens[(i, j)].add_analysis(analysis=analysis)
+            else:
+                tokens[(i, j)] = Token.from_analysis(
+                    start=i, end=j, analysis=analysis)
+
+        return [token for pos, token in tokens.items()]
+
+    def analyse_text(self, text: str):
+        return self._morfeusz.analyse(text)
--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -1,4 +1,59 @@
-from entity_linking.parser import parser
+from pytest import fixture
+from morfeusz2 import Morfeusz
+from entity_linking.parser.parser import Parser
+from entity_linking.grammar.range import Range
+from entity_linking.grammar.token import Token
 
-def test_simple():
-    assert True
+
+@fixture(scope="module")
+def parser():
+    return Parser()
+
+
+@fixture(scope="module")
+def simple_analysis_word():
+    return "11-latkowie"
+
+
+@fixture(scope="module")
+def simple_analysis_token():
+    return Token.from_analysis(start=0, end=1, analysis=(
+        '11-latkowie', '11-latek', 'subst:pl:nom.voc:m1', ['nazwa pospolita'], []))
+
+
+@fixture(scope="module")
+def double_analysis_word():
+    return "Zaginieni"
+
+
+@fixture(scope="module")
+def double_analysis_token():
+    token = Token.from_analysis(start=0, end=1, analysis=(
+        'Zaginieni', 'zaginiony:a', 'adj:pl:nom.voc:m1:pos', [], []))
+    token.add_analysis(('Zaginieni', 'zaginiony:s',
+                        'subst:pl:nom.voc:m1', ['nazwa_pospolita'], []))
+    return token
+
+
+def test_analyse_text(parser):
+    word = "tests"
+    assert parser.analyse_text(word) == Morfeusz().analyse(word)
+
+
+def test_parse_simple_analysis_word(parser, simple_analysis_token, simple_analysis_word):
+    tokens = parser.parse_text(simple_analysis_word)
+    assert len(tokens) == 1
+    assert tokens[0] == simple_analysis_token
+
+
+def test_parse_double_analysis_word(parser, double_analysis_word, double_analysis_token):
+    tokens = parser.parse_text(double_analysis_word)
+    assert len(tokens) == 1
+    assert tokens[0] == double_analysis_token
+
+def test_multiple_word_analysis(parser, double_analysis_token, double_analysis_word, simple_analysis_token, simple_analysis_word):
+    tokens = parser.parse_text(double_analysis_word + ' ' + simple_analysis_word)
+    assert len(tokens) == 2
+    assert tokens[0] == double_analysis_token
+    simple_analysis_token._range = Range(start=1, end=2)
+    assert tokens[1] == simple_analysis_token
