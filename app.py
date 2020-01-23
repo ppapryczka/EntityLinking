d7619ad13ad48bdf4fa40ce828d49ae94d885b08
ppapryczka <patryk.pankiewicz369@gmail.com>
2020-01-23 15:00:03 +0100

    App fix.

--- app.py
+++ app.py
@@ -1,24 +1,65 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-from entity_linking import wikidata_api as api
-from entity_linking import wikidata_graph as graph
+# run test
+from entity_linking.classification_report import create_report_for_result
+from entity_linking.wikidata_api import WikidataAPI, WikidataWebAPI, WikidataDBAPI
+from entity_linking.entity_classifier import WikipediaContextGraphEntityClassifier
+from entity_linking.tokenizer import WikidataMorphTagsTokenizer
 
-from entity_linking.maintenance.logger import get_logger
+
+import sys
+
+
+def run_test_command(input_file: str, seq_number: int, database_name: str):
+    api: WikidataAPI
+
+    if database_name != "":
+        api = WikidataDBAPI(database_name)
+    else:
+        api = WikidataWebAPI()
+
+    tokenizer = WikidataMorphTagsTokenizer(api, 2)
+    graph_classifier = WikipediaContextGraphEntityClassifier(tokenizer, api, 5, 8)
+
+    result = graph_classifier.classify_sequences_from_file(
+        input_file, seq_number
+    )
+
+    create_report_for_result(result,
+        seq_number,
+        input_file,
+        f"classifier:{WikidataMorphTagsTokenizer.__name__}, "
+        f"tokenizer: {WikipediaContextGraphEntityClassifier.__name__}")
 
 
 def get_args_parser() -> ArgumentParser:
+
     parser = ArgumentParser(description='Entity Linking',
                             formatter_class=ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('mode',
-                        type=int,
-                        help="App mode:0(train), 1(test), 2(run)")
+    subparsers = parser.add_subparsers()
 
-    parser.add_argument('-i', '--input', required=True, type=str,
+    test_parser = subparsers.add_parser("test")
+
+    test_parser.add_argument(
+        '-i', '--input', required=True, type=str, help="Input file"
+    )
+    test_parser.add_argument(
+        '-N', '--num', required=True, type=int, help="Sequences number"
+    )
+    test_parser.add_argument(
+        '-db', type=str, required=False, default="", help="Path to database",
+    )
+
+    '''
+    run_parser = subparsers.add_parser("run")
+
+    run_parser.add_argument('-i', '--input', required=True, type=str,
                         help="Input file")
 
-    parser.add_argument('-o', '--output', required=True, type=str,
+    run_parser.add_argument('-o', '--output', required=True, type=str,
                         help="Output file")
+    '''
 
     parser.set_defaults(func=lambda x: parser.print_help())
 
@@ -26,14 +67,11 @@ def get_args_parser() -> ArgumentParser:
 
 
 def main():
-    logger = get_logger("app")
     parser = get_args_parser()
-    args = parser.parse_args()
-    mode = args.mode
-    input_file = args.input
-    output_file = args.output
-    logger.info("Mode: {}, input file: {}, output file: {}".format(
-        mode, input_file, output_file))
+    print(sys.argv[1:])
+    args = parser.parse_args(sys.argv[1:])
+
+    run_test_command(args.input, args.num, args.db)
 
 
 if __name__ == "__main__":
